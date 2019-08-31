9b690b0717a283bad88ba969728228c721e02f94
GY <e0310259@u.nus.edu>
2019-08-31 20:09:53 +0800

    Level 9: Find

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,102 +10,10 @@ public class Duke {
      * Why checkstyle liddat.
      */
     
-    static void addToFile(String directory, int position, String type, String content, String content2) {
-        String line = "____________________________________________________________\n";
-        try (BufferedWriter update = new BufferedWriter(new FileWriter(directory, true))) {
-            String fileContent = position + " | false | " + type + " | " + content + " | " + content2 + "\n";
-            update.append(fileContent);
-            update.close();
-            
-        }
-        catch (IOException e) {
-            System.out.println(line + "Something's wrong with saving the file!\n" + line);
-        }
-    }
-    
-    /*static void modifyFile(String directory, String oldstring, String newstring) {
-        try {
-                BufferedReader reader = new BufferedReader(new FileReader(directory));
-                String oldContent = "";
-                //Reading all the lines of input text file into oldContent
-
-                String line = reader.readLine();
-
-                while (line != null) {
-                    oldContent = oldstring + line + System.lineSeparator();
-                    line = reader.readLine();
-                }
-
-                String newContent = oldContent.replace(oldstring, newstring);
-                BufferedWriter update = new BufferedWriter(new FileWriter(directory));
-                update.write(newContent);
-                try
-                {
-                    reader.close();
-                    update.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            catch (FileNotFoundException e) {
-                System.out.println("File is missing??");
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-    }
-*/
-    static void modifyFile(String filePath, String oldString, String newString) {
-        File fileToBeModified = new File(filePath);
-        String oldContent = "";
-        BufferedReader reader = null;
-        FileWriter writer = null;
-        
-        try {
-            reader = new BufferedReader(new FileReader(fileToBeModified));
-            
-            //Reading all the lines of input text file into oldContent
-            
-            String line = reader.readLine();
-            
-            while (line != null) {
-                oldContent = oldContent + line + System.lineSeparator();
-                line = reader.readLine();
-            }
-            
-            //Replacing oldString with newString in the oldContent
-            
-            String newContent = oldContent.replace(oldString, newString);
-            
-            //Rewriting the input text file with newContent
-            
-            writer = new FileWriter(fileToBeModified);
-            
-            writer.write(newContent);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                //Closing the resources
-                
-                reader.close();
-                
-                writer.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String directory = "C:\\Users\\GY\\Downloads\\CS2113T\\duke\\data\\duke.txt";
         Scanner input = new Scanner(System.in);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HHmm");
         dateFormat.setLenient(false);
         String line = "____________________________________________________________\n";
         ArrayList<Task> tasklist = new ArrayList<Task>();
@@ -123,32 +31,42 @@ public class Duke {
                     System.out.println(line + "☹ OOPS!!! The description of a todo cannot be empty.\n" + line);
                     continue;
                 }
-                Task t = new Todo(typing.substring(indexOfSpace + 1));
+                String description = typing.substring(indexOfSpace + 1);
+                Task t = new Todo(description);
                 tasklist.add(t);
                 System.out.println(line + "Got it. I've added this task: \n" + "  " + t);
                 System.out.println("Now you have " + tasklist.size() + " task(s) in the list.\n" + line);
                 
-                addToFile(directory, tasklist.size(), "T", typing.substring(indexOfSpace + 1), null);
-            }
-            else if (instruction.equals("deadline")) {
                 try {
-                    int indexOfSlash = typing.indexOf("/by");
-                    if (indexOfSlash == -1)
-                    {
-                        System.out.println(line + "☹ OOPS!!! Please use /by\n" + line);
-                        continue;
-                    }
+                    Editor p = new Editor("T", description);
+                    p.addToFile();
+                }
+                catch (IOException e) {
+                    System.out.println(line + "☹ OOPS!!! Something's wrong with the file!\n" + line);
+                }
+            }
+            
+            
+            else if (instruction.equals("deadline")) {
+                int indexOfSlash = typing.indexOf("/by");
+                if (indexOfSlash == -1)
+                {
+                    System.out.println(line + "☹ OOPS!!! Please use /by\n" + line);
+                    continue;
+                }
+                
+                try {
+                    String description = typing.substring(indexOfSpace + 1, indexOfSlash - 1);
+                    String time = typing.substring(indexOfSlash + 4);
                     Date date = dateFormat.parse(typing.substring(indexOfSlash + 4));
-                    if (!typing.substring(indexOfSlash + 1, indexOfSlash + 3).equals("by")) {
-                        System.out.println(line + "☹ OOPS!!! Please enter a valid deadline input.\n" + line);
-                        continue;
-                    }
-                    Task t = new Deadline(typing.substring(indexOfSpace + 1, indexOfSlash), typing.substring(indexOfSlash + 4));
+                    
+                    Task t = new Deadline(description, time);
                     tasklist.add(t);
                     System.out.println(line + "Got it. I've added this task: \n" + t);
                     System.out.println("Now you have " + tasklist.size() + " task(s) in the list.\n" + line);
+                    Editor p = new Editor("D", description, time);
+                    p.addToFile();
                     
-                    addToFile(directory, tasklist.size(), "D", typing.substring(indexOfSpace + 1, indexOfSlash), typing.substring(indexOfSlash + 4));
                 }
                 catch (StringIndexOutOfBoundsException e) {
                     System.out.println(line + "☹ OOPS!!! Please enter a valid deadline input.\n" + line);
@@ -156,23 +74,30 @@ public class Duke {
                 catch (ParseException e) {
                     System.out.println(line + "☹ OOPS!!! Please enter a valid date! dd-MMM-yyyy HH:mm:ss \n" + line);
                 }
+                catch (IOException e) {
+                    System.out.println(line + "☹ OOPS!!! Something's wrong with the file!\n" + line);
+                }
             }
+            
+            
             else if (instruction.equals("event")) {
+                int indexOfSlash = typing.indexOf("/at");
+                if (indexOfSlash == -1)
+                {
+                    System.out.println(line + "☹ OOPS!!! Please use /at\n" + line);
+                    continue;
+                }
+                
                 try {
-                    int indexOfSlash = typing.indexOf("/at");
-                    if (indexOfSlash == -1)
-                    {
-                        System.out.println(line + "☹ OOPS!!! Please use /at\n" + line);
-                        continue;
-                    }
+                    String description = typing.substring(indexOfSpace + 1, indexOfSlash - 1);
+                    String time = typing.substring(indexOfSlash + 4);
                     Date date = dateFormat.parse(typing.substring(indexOfSlash + 4));
-                    Task t = new Event(typing.substring(indexOfSpace + 1, indexOfSlash), typing.substring(indexOfSlash + 4));
+                    Task t = new Event(description, time);
                     tasklist.add(t);
                     System.out.println(line + "Got it. I've added this task: \n" + t);
                     System.out.println("Now you have " + tasklist.size() + " task(s) in the list.\n" + line);
-    
-                    addToFile(directory, tasklist.size(), "E", typing.substring(indexOfSpace + 1, indexOfSlash), typing.substring(indexOfSlash + 4));
-    
+                    Editor p = new Editor("E", description, time);
+                    p.addToFile();
                 }
                 catch (StringIndexOutOfBoundsException e) {
                     System.out.println(line + "☹ OOPS!!! Please enter a valid event input.\n" + line);
@@ -180,22 +105,39 @@ public class Duke {
                 catch (ParseException e) {
                     System.out.println(line + "☹ OOPS!!! Please enter a valid date! dd-MMM-yyyy HH:mm:ss \n" + line);
                 }
+                catch (IOException e) {
+                    System.out.println(line + "☹ OOPS!!! Something's wrong with the file!\n" + line);
+                }
             }
+            
+            
             else if (instruction.equals("done")) {
                 try {
                     int temp = Integer.parseInt(typing.substring(5));
                     tasklist.get(temp - 1).done();
-                    System.out.print(line);
-                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(line + "Nice! I've marked this task as done: ");
                     System.out.println(tasklist.get(temp - 1) + "\n" + line);
+                    Editor p = new Editor();
+                    p.markAsDone(temp);
                 }
-                catch (NumberFormatException | StringIndexOutOfBoundsException e) {
-                    System.out.println(line + "Which task?\n" + line);
-                    continue;
+                catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println(line + "Please enter a valid task!\n" + line);
                 }
-                
-                modifyFile(directory, typing.substring(5) + " | false | ", typing.substring(5) + " | true | ");
+                catch (IOException e) {
+                    System.out.println(line + "☹ OOPS!!! Something's wrong with the file!\n" + line);
+                }
             }
+            
+            
+            else if (instruction.equals("clear"))
+            {
+                System.out.println(line + "List has been cleared!\n" + line);
+                Editor p = new Editor();
+                p.clear();
+                tasklist.clear();
+            }
+            
+            
             else if (instruction.equals("list")) {
                 System.out.println(line + "Here are the tasks in your list: ");
                 for (int i = 0; i < tasklist.size(); i += 1) {
@@ -204,6 +146,22 @@ public class Duke {
                 }
                 System.out.print(line);
             }
+            
+            
+            else if (instruction.equals("find"))
+            {
+                Editor p = new Editor();
+                ArrayList<Integer> found = p.find(typing.substring(5));
+
+                System.out.println(line + "Here are the matching tasks in your list:");
+                for (int i = 1; i <= found.size(); i += 1)
+                {
+                    System.out.println(i + ". " + tasklist.get(found.get(i - 1)));
+                }
+                System.out.println(line);
+            }
+            
+            
             else if (instruction.equals("bye")) {
                 break;
             }
---
+++ src/main/java/Editor.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Editor {
+    protected String type;
+    protected String description;
+    protected String date = "";
+    public String line = "____________________________________________________________\n";
+    public String directory = "C:\\Users\\GY\\Downloads\\CS2113T\\duke\\data\\duke.txt";
+    
+    public Editor () {
+    
+    }
+    
+    public Editor(String type, String description) throws IOException {
+        this.type = type;
+        this.description = description;
+    }
+    
+    public Editor(String type, String description, String date) throws IOException {
+        this.type = type;
+        this.description = description;
+        this.date = date;
+    }
+    
+    public void addToFile () throws IOException {
+        BufferedWriter update = new BufferedWriter(new FileWriter(directory, true));
+        String fileContent = type + "|false|" + description + "|" + date + "\n";
+        update.append(fileContent);
+        update.close();
+    }
+    
+    public void markAsDone (int taskNo) throws IOException {
+        String content = "";
+        File fileToBeModified = new File(directory);
+        BufferedReader reader = new BufferedReader(new FileReader(fileToBeModified));
+        
+        int counter = 1;
+        String line = reader.readLine();
+        while (line != null) {
+            if (counter == taskNo)
+            {
+                line = line.replace("false", "true");
+            }
+            content = content + line + System.lineSeparator();
+            line = reader.readLine();
+            counter += 1;
+        }
+        
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileToBeModified));
+        writer.write(content);
+        
+        reader.close();
+        writer.close();
+    }
+    
+    
+    public void clear() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(directory));
+        writer.write("");
+        writer.close();
+    }
+    
+    public ArrayList<Integer> find(String word) throws IOException {
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        String content = "";
+        File fileToBeModified = new File(directory);
+        BufferedReader reader = new BufferedReader(new FileReader(fileToBeModified));
+        
+        int counter = 0;
+        String line = reader.readLine();
+        while (line != null) {
+            if (line.contains(word))
+            {
+                row.add(counter);
+            }
+            line = reader.readLine();
+            counter += 1;
+        }
+        
+        return row;
+    }
+}
